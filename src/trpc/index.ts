debd02973a5732a4b1dcbfc07283e7843ae9a160
omark01 <omark012000@gmail.com>
2024-02-01 10:11:54 -0500

    added end point to create a trip

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,10 +1,34 @@
 // API
+import { db } from "@/db";
 import { publicProcedure, router } from "./trpc";
-
+import { tripInfoInput } from "@/types";
+import type { Vehicle } from "@prisma/client";
 export const appRouter = router({
-    test:publicProcedure.query(()=>{
-        return "hello trpc"
-    })
+  test: publicProcedure.query(() => {
+    return "hello trpc";
+  }),
+  getVehicles: publicProcedure.query(async () => {
+    const vehicles = await db.vehicle.findMany();
+    return vehicles;
+  }),
+  createTrip: publicProcedure
+    .input(tripInfoInput)
+    .mutation(async ({ ctx, input }) => {
+      const trip = await db.trip.create({
+        data: {
+          destination: "Dummy Destination",
+          origin: "Dummy Origin",
+          status: "CANCELLED", // Assuming "Scheduled" is a valid status enum value
+          startTime: new Date().toISOString(), // Current time
+          endTime: new Date().toISOString(), // Current time
+          scheduledAt: new Date().toISOString(), // Current time
+          rating: 5, // A dummy rating value
+          price: 49.99, // A dummy price value
+          vehicleId: 1, // Assuming 1 is a valid vehicle ID
+          userId: "dummyUser123", // Assuming "dummyUser123" is a
+        },
+      });
+    }),
 });
 
 export type AppRouter = typeof appRouter;
--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,20 @@
-import { initTRPC } from "@trpc/server/unstable-core-do-not-import";
+import { useUser } from "@clerk/nextjs";
+import { TRPCError, initTRPC } from "@trpc/server/unstable-core-do-not-import";
 
 const t = initTRPC.create();
+const middlware = t.middleware;
 
+const isAuth = middlware(async (opts) => {
+  const { user } = useUser();
+  if (!user || !user.id) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return opts.next({
+    ctx: {
+      userId: user.id,
+      user,
+    },
+  });
+});
 export const router = t.router;
 export const publicProcedure = t.procedure;
