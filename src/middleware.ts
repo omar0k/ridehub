690198f3546365df0d485bd1876c169518049cd5
omark01 <omark012000@gmail.com>
2024-02-10 00:13:24 -0500

    build fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+export default authMiddleware({});
 export const config = {
   matcher: ["/dashboard/:path*"],
 };
-export default authMiddleware({
-  async handler(req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode = 200;
-    res.end(JSON.stringify({ message: "Authenticated" }));
-  },
-});
