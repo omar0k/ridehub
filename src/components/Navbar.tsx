b790118219e81bebd66ee38fea80dece8937470e
omark01 <omark012000@gmail.com>
2024-02-24 20:24:10 -0500

    added margin to the bottom of the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ import { Icons } from "./Icons";
 const Navbar = () => {
   const { user } = useUser();
   return (
-    <nav className="sticky inset-x-0 top-0 z-30 h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+    <nav className="sticky inset-x-0 top-0 z-30 mb-10 h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <ClerkLoaded>
           <div className="flex h-14 items-center justify-between border-b border-zinc-200">
