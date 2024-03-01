5bdc90ca6b4ff158f8ff713afdb6d310b88717f3
omark01 <omark012000@gmail.com>
2024-03-01 03:19:03 -0500

    redirect is now handled by the middleware

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,14 @@
 "use client";
-import { RedirectToSignUp, auth, useUser } from "@clerk/nextjs";
-import { Loader2 } from "lucide-react";
-import { redirect } from "next/navigation";
-import { useEffect } from "react";
+import { useUser } from "@clerk/nextjs";
+// @TODO:
+/*
+1.show all user trips inside of dashboard 
+2.have button to book trip
+3.allow user to have their default information such as pick up, drop off
 
-export default function AdminDashboard() {
-  const { user } = useUser();
-  if (!user) {
-    <RedirectToSignUp />;
-  }
+*/
+export default function Dashboard() {
+  const { user, isSignedIn } = useUser();
   return (
     <>
       <h1>This is the user dashboard</h1>
