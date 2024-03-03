6ddb416ebd8632a3af7329a954ecdb70a0680736
omark01 <omark012000@gmail.com>
2024-03-03 17:35:41 -0500

    created a trips table component that displays the trips/shadcn

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,20 @@
 "use client";
-import { useUser } from "@clerk/nextjs";
-import { trpc } from "../_trpc/client";
-import { useState } from "react";
+import UserTrips from "@/components/UserTrips";
 // @TODO:
 /*
-1.show all user trips inside of dashboard 
+1.show all user trips inside of dashboard DONE
 2.have button to book trip
 3.allow user to have their default information such as pick up, drop off
 
 */
 export default function Dashboard() {
-  const { user, isSignedIn } = useUser();
-  const [trips, setTrips] = useState([]);
-  const { data, isLoading } = trpc.getTripsByUserId.useQuery();
-  console.log(data);
   return (
-    <>
-      <div>{data?.map((trip) => <div>{trip.id}</div>)}</div>
-      <h1>This is the user dashboard</h1>
-    </>
+    <div>
+      {/* <LeftSideMenu */}
+      {/* 
+        Saved preferences 
+      */}
+      <UserTrips />
+    </div>
   );
 }
---
+++ src/components/UserTrips.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { tripInfoInput } from "@/types";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { trpc } from "@/app/_trpc/client";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import clsx from "clsx";
+// @TODO:
+/*
+1.show all user trips inside of dashboard 
+2.have button to book trip
+3.allow user to have their default information such as pick up, drop off
+
+*/
+const UserTrips = () => {
+  const { data: trips, isLoading } = trpc.getTripsByUserId.useQuery();
+  if (isLoading) {
+    return (
+      <div className="rounded-lg px-10">
+        <Skeleton className="rounded-lg" count={3} height={100} />;
+      </div>
+    );
+  }
+  return (
+    <Card className="mx-10 border-gray-300">
+      <CardHeader className="flex flex-row items-center justify-between px-40">
+        <p className="flex-grow text-center text-2xl font-semibold">Trips</p>
+        <Link
+          className={clsx(
+            buttonVariants(),
+            "group flex items-center justify-center gap-2 ",
+          )}
+          href={"/"}
+        >
+          Book Trip
+          <ArrowRight className="group-hover:translate-x-2 transition-all duration-300" />
+        </Link>
+      </CardHeader>
+
+      <CardContent>
+        <Table className=" ">
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>From</TableHead>
+              <TableHead>To</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead>Time</TableHead>
+              <TableHead>Price</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {trips?.map((trip, idx) => {
+              return (
+                <TableRow key={trip.id}>
+                  <TableCell className="opacity-60">{idx + 1}</TableCell>
+                  <TableCell>{trip.origin}</TableCell>
+                  <TableCell>{trip.destination}</TableCell>
+                  <TableCell>{trip.scheduleDate}</TableCell>
+                  <TableCell>{trip.scheduleTime}</TableCell>
+                  <TableCell>${trip.price}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+};
+export default UserTrips;
