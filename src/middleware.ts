e59e8e7b23866f23184e2c8ec26553dad51b76f0
omark01 <omark012000@gmail.com>
2024-03-19 03:21:45 -0400

    added publicroutes to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,16 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks(.*)", "/", "/fleet","/api/trpc/getVehicles"],
+  publicRoutes: [
+    "/api/webhooks(.*)",
+    "/",
+    "/fleet",
+    "/api/trpc/getVehicles",
+    "/api/trpc/createTrip",
+    "/api/trpc/createStripeSession",
+    "/payment-successful/(.*)",
+    "/api/trpc/getTrip",
+  ],
 });
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
