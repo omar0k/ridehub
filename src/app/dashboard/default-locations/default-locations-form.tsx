1dcdf46341b80ba7514110524ea524b7742ea01c
omark01 <omark012000@gmail.com>
2024-04-21 01:16:56 -0400

    added api calls to update user default locations with responsive toasts

--- src/app/dashboard/default-locations/default-locations-form.tsx
+++ src/app/dashboard/default-locations/default-locations-form.tsx
@@ -10,32 +10,64 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { DefaultLocationsFormData } from "@/types";
+import { useToast } from "@/components/ui/use-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Autocomplete from "react-google-autocomplete";
-import { UseFormReturn, useForm } from "react-hook-form";
-import { TypeOf, z } from "zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 const DefaultLocationsSchema = z.object({
   defaultPickUpLocation: z.string(),
   defaultDropOffLocation: z.string(),
 });
 
 const DefaultLocationsForm = () => {
+  const { toast } = useToast();
   const { data: user } = trpc.getUser.useQuery();
+  const { mutate: updateUserDefaultLocations, data: updatedUser } =
+    trpc.updateUserDefaultLocations.useMutation({
+      onSuccess: (data) => {
+        return toast({
+          title: "Updated successfully!",
+          description: (
+            <div className="flex flex-col">
+              <span>
+                <span className="font-bold">PickUp:</span>{" "}
+                {data?.savedPickUpLocation}
+              </span>
+              <span>
+                <span className="font-bold">DropOff:</span>{" "}
+                {data?.savedDropOffLocation}
+              </span>
+            </div>
+          ),
+        });
+      },
+      onError: (error) => {
+        return toast({
+          variant: "destructive",
+          title: "Error updating",
+          description: "There was an error updating, please try again.",
+        });
+      },
+    });
   const form = useForm<z.infer<typeof DefaultLocationsSchema>>({
     resolver: zodResolver(DefaultLocationsSchema),
     defaultValues: {
       defaultDropOffLocation: user?.savedDropOffLocation
         ? user.savedDropOffLocation
         : "",
-      defaultPickUpLocation: user?.savedDropOffLocation
-        ? user.savedDropOffLocation
+      defaultPickUpLocation: user?.savedPickUpLocation
+        ? user.savedPickUpLocation
         : "",
     },
   });
-  const handleSubmit = (fields: z.infer<typeof DefaultLocationsSchema>) => {
-    console.log(fields);
+  const handleSubmit = async (
+    fields: z.infer<typeof DefaultLocationsSchema>,
+  ) => {
+    await updateUserDefaultLocations({
+      dropOffLocation: fields.defaultDropOffLocation,
+      pickUpLocation: fields.defaultPickUpLocation,
+    });
   };
   return (
     <div className="flex h-full  flex-grow-0 flex-col">
@@ -50,12 +82,45 @@ const DefaultLocationsForm = () => {
           >
             <FormField
               control={form.control}
+              name="defaultPickUpLocation"
+              render={({ field }) => {
+                return (
+                  <div>
+                    <FormItem>
+                      <FormLabel>Pick Up</FormLabel>
+                      <FormControl>
+                        <Autocomplete
+                          className="custom-input"
+                          {...field}
+                          apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+                          onPlaceSelected={(place) => {
+                            if (place.formatted_address) {
+                              form.setValue(
+                                "defaultPickUpLocation",
+                                place.formatted_address,
+                              );
+                            }
+                          }}
+                          options={{
+                            componentRestrictions: { country: "US" },
+                            types: ["geocode", "establishment"],
+                          }}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  </div>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
               name="defaultDropOffLocation"
               render={({ field }) => {
                 return (
                   <div className="">
                     <FormItem>
-                      <FormLabel>Pick Up</FormLabel>
+                      <FormLabel>Drop Off</FormLabel>
                       <FormControl>
                         <Autocomplete
                           className="custom-input"
@@ -82,42 +147,9 @@ const DefaultLocationsForm = () => {
                 );
               }}
             />
-            <FormField
-              control={form.control}
-              name="defaultPickUpLocation"
-              render={({ field }) => {
-                return (
-                  <div>
-                    <FormItem>
-                      <FormLabel>Drop Off</FormLabel>
-                      <FormControl>
-                        <Autocomplete
-                          className="custom-input"
-                          {...field}
-                          apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
-                          onPlaceSelected={(place) => {
-                            if (place.formatted_address) {
-                              form.setValue(
-                                "defaultPickUpLocation",
-                                place.formatted_address,
-                              );
-                            }
-                          }}
-                          options={{
-                            componentRestrictions: { country: "US" },
-                            types: ["geocode", "establishment"],
-                          }}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  </div>
-                );
-              }}
-            />
             <div className="flex items-center">
               <Button type="submit" size={"lg"}>
-                Save
+                Update
               </Button>
             </div>
           </form>
--- src/app/dashboard/default-locations/page.tsx
+++ src/app/dashboard/default-locations/page.tsx
@@ -3,7 +3,6 @@ import DefaultLocationsForm from "./default-locations-form";
 export default function DefaultLocationsPage() {
   return (
     <div>
-      
       <DefaultLocationsForm />
     </div>
   );
